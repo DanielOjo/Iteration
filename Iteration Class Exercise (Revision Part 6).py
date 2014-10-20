76d330119e2aa9b93b5730f6ed6c283403471a1c
DanielOjo <danielojo@live.com>
2014-10-21 00:25:52 +0100

    Updated

--- Iteration Class Exercise (Revision Part 6).py
+++ Iteration Class Exercise (Revision Part 6).py
@@ -2,7 +2,8 @@
 #19-10-2014
 #Iteration Class Exercise (Revision Part 6)
 
-for pound_weight in range(1,21):
-    kilograms_weight = pound_weight*2.2
-    print("Pounds",pound_weight)
+for kilograms_weight in range(1,21):
+    pound_weight = kilograms_weight*2.2
     print("Kilograms",kilograms_weight)
+    print("Pounds",pound_weight)
+   
