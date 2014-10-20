4b9526f2864ce96261e6643179d095f2f101c6a6
DanielOjo <danielojo@live.com>
2014-10-20 12:16:08 +0100

    done in class

---
+++ Classroom exercises/Revision exercise 1.py
@@ -0,0 +1,9 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #Daniel Ogunlana
+>>> #20-10-2014
+>>> #Iteration Class Exercises
+>>> #Revision Exericise 1
+>>> 
+>>> for count in range (1,10):
+    print("Hello")
---
+++ Classroom exercises/revision exericise 1.py
@@ -0,0 +1,7 @@
+#Daniel Ogunlana
+#20-10-2014
+#Iteration Class Exercises
+#Revision Exericise 1
+
+for count in range (1,11):
+    print("Hello")
