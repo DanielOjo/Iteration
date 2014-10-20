2931e339df0d38d960e0515452f54424f52bfb50
DanielOjo <danielojo@live.com>
2014-10-21 00:21:11 +0100

    Finished it

--- Iteration Class Exercise (Revision Part 6).py
+++ Iteration Class Exercise (Revision Part 6).py
@@ -2,6 +2,7 @@
 #19-10-2014
 #Iteration Class Exercise (Revision Part 6)
 
-pound_weight = int(input("Please enter an amount of pounds that you want to convert to kilograms: "))
-kilograms_weight = ("{0}"*2.2.format(pound_weight))
-print(kilograms_weight)
+for pound_weight in range(1,21):
+    kilograms_weight = pound_weight*2.2
+    print("Pounds",pound_weight)
+    print("Kilograms",kilograms_weight)
