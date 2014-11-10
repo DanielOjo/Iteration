d030232b32aac184695bce251bacbd6c40f97543
DanielOjo <danielojo@live.com>
2014-11-10 12:17:29 +0000

    Done in class

---
+++ Guess The Number Game.py
@@ -0,0 +1,12 @@
+#Daniel Ogunlana, Danish Ali
+#10/11/14
+#Guess The Number Game
+
+#import random
+#random.randint(1,100)
+
+random_number = 72
+total = 0
+
+while random_number == random_number:
+    
